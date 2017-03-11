145048c4da51dc6f18f198b8c78b842edfa4cf46
rawlingsj <rawlingsj80@gmail.com>
2017-03-11 22:08:37 +0000

    release it

--- release.groovy
+++ release.groovy
@@ -35,11 +35,11 @@ def cd (b){
         sh 'chmod 600 /root/.ssh-git/ssh-key'
         sh 'chmod 600 /root/.ssh-git/ssh-key.pub'
         sh 'chmod 700 /root/.ssh-git'
-
+        sh 'cp /home/jenkins/.npm/.npmrc /home/jenkins/.npmrc'
         sh 'npm version patch'
         sh 'git push origin master --tags'
-        input id: 'Proceed', message: "ok"
-        
+        //input id: 'Proceed', message: "ok"
+
         sh 'npm publish'
         //sh "GIT_BRANCH=${b} npm run semantic-release"
     }
